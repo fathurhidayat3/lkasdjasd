b366a57868e48e4deca78a3cb18e25018d8c77fb
fathurhidayat3 <fathur.hidayat@gmail.com>
2020-09-19 12:08:30 +0700

    ch

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reactaslkjdhajkdlahsd
+          Learnasdasdasdasd Reactaslkjdhajkdlahsd
         </a>
       </header>
     </div>
